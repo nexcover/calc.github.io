2eb19867bf80050ffcd6664345aa97f8afb58a26
LeeSeungGook <hayangbal@gmail.com>
2024-04-03 01:26:46 +0900

    Add files via upload

--- main.js
+++ main.js
@@ -16,20 +16,26 @@ function dot() {
 }
 
 function calculate(operator) {
-    // 입력된 값이 숫자인지 확인
-    if (!isNaN(input)) {
-        // 숫자라면 100을 곱함
-        input = parseFloat(input) * 100;
-        document.getElementById("inputArea").value = input;
-        updateOutput();
+    // '%' 연산자일 경우
+    if (operator === '%') {
+        if (!isNaN(input)) {
+            // 숫자라면 100을 곱함
+            input = parseFloat(input) * 100;
+            document.getElementById("inputArea").value = input;
+            updateOutput();
+        } else {
+            // 숫자가 아닐 경우 기존 로직을 수행
+            input += operator;
+            document.getElementById("inputArea").value = input;
+            updateOutput();
+        }
     } else {
-        // 숫자가 아닐 경우 기존 로직을 수행
+        // 일반 연산자일 경우
         input += operator;
         document.getElementById("inputArea").value = input;
         updateOutput();
     }
 }
-
 
 function equal() {
     try {
